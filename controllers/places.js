4c55407b7dec1651a1738b08faaf2c12e649f6b8
unknown <jessespeks@hotmail.com>
2022-08-16 20:32:12 -0400

    up to part V rest rant

---
+++ controllers/places.js
@@ -0,0 +1,27 @@
+const router = require('express').Router()
+const places = require('../models/places.js')
+//get the route that will eventually show the list of places
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+
+module.exports = router
