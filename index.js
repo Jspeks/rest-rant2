bc7579afd3de50bc643eb4115cdefa60ac397c81
unknown <jessespeks@hotmail.com>
2022-08-16 23:10:30 -0400

    throwing an error, will continue tomorrow

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 // Require needed modules.
 const express = require('express')
+const methodOverride = require('method-override')
 
 // Initialize the app object.
 const app = express()
@@ -12,6 +13,7 @@ app.engine('jsx', require('express-react-views').createEngine())
 //calls your static folder
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 //import the router
 app.use('/places', require('./controllers/places'))
