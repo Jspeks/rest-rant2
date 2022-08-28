81f779dd881f32e797dcb4bfc6be277eb2d2e76b
unknown <jessespeks@hotmail.com>
2022-08-28 16:19:34 -0400

    Updating rating system

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,6 +7,22 @@ function show (data) {
             No Comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
+    if (data.place.comments.length)
+    {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+        return tot + c.stars
+    }, 0)
+    let averageRating = sumRatings / data.place.comments.length 
+    rating = (
+        <h3>
+        {Math.round{averageRating}} stars
+        </h3>
+    )
     comments = data.place.comments.map(c => {
         return (
             <div className="border">
@@ -19,6 +35,7 @@ function show (data) {
             </div>
         )
     })
+}
     return (
         <Def>
             <main>
@@ -31,7 +48,8 @@ function show (data) {
                     </div>
                     <div className="col-sm-6">
                         <h1>{ data.place.name }</h1>
-                        <h2>Currently unrated</h2>
+                        <h2>Rating</h2>
+                        {rating}
                         <h3>
                             {data.place.showEstablished()}
                         </h3>
